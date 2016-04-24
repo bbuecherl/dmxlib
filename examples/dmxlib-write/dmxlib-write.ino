b3781aba74bfa869f84d2f4cf8b72556f312a8bd
Bernhard Bücherl <bernhard.buecherl@gmail.com>
2016-04-24 21:45:10 +0200

    improved write example

--- examples/dmxlib-write/dmxlib-write.ino
+++ examples/dmxlib-write/dmxlib-write.ino
@@ -1,36 +1,52 @@
 #include "dmxlib.h"
 
 #define PIN_DMX 11
+#define PIN_RED 2
+#define PIN_YELLOW 3
+#define PIN_GREEN 4
 
 DMXLib::DMXPWMWriter writer;
 uint8_t x = 0;
 int8_t y = 1;
+uint8_t value = 0;
+uint16_t channel = 0;
 
 void setup() {
   Serial.begin(115200);
   writer.setChannels(512);
   writer.use(PIN_DMX);
   writer.start();
-  writer.set(5, 36);
-  writer.set(6, 5);
-  writer.set(7, 100);
-  writer.set(12, 1);
+
+  pinMode(PIN_RED, OUTPUT);
+  pinMode(PIN_YELLOW, OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+
+  digitalWrite(PIN_GREEN, HIGH);
 }
 
 void loop() {
-  if(x > 254) {
-    y = -1;
-  } else if(x < 1) {
-    y = 1;
-  }
-  x += y;
-  writer.set(0, x);
-  writer.set(1, x);
-  writer.set(5, x);
+  if(Serial.available() > 0) {
+    digitalWrite(PIN_YELLOW, HIGH);
+    uint16_t c = Serial.read();
 
-  delay(4);
+    if ((c >= '0') && (c <= '9')) {
+      value = 10*value + c - '0';
+    } else {
+      if (c == 'c') channel = value;
+      else if (c == 'w') {
+        writer.set(channel, value);
+      }
+      value = 0;
+    }
+  } else {
+    digitalWrite(PIN_YELLOW, LOW);
+  }
 }
 
+bool toggle = 0;
+
 ISR_FUNCTION() {
+  digitalWrite(PIN_RED, toggle ? HIGH : LOW);
+  toggle = toggle ? 0 : 1;
   writer.write();
 }
